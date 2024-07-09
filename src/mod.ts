1b86fc742fb153ec4e07d3ba82575c4f58506464
mBarneto <4347791+m-barneto@users.noreply.github.com>
2024-07-09 02:07:36 -0400

    :wastebasket:

---
+++ src/mod.ts
@@ -0,0 +1,16 @@
+import { DependencyContainer } from "tsyringe";
+import { IPreSptLoadMod } from "@spt/models/external/IPreSptLoadMod";
+import { InraidControllerExtension } from "./InraidControllerExtension";
+import { ILogger } from "@spt/models/spt/utils/ILogger";
+
+class Mod implements IPreSptLoadMod {
+
+    preSptLoad(container: DependencyContainer): void {
+        const logger = container.resolve<ILogger>("WinstonLogger");
+        logger.info("PreSPTLoad!");
+        container.register<InraidControllerExtension>("InraidControllerExtension", InraidControllerExtension);
+        container.register("InraidController", { useToken: "InraidControllerExtension" });
+    }
+}
+
+export const mod = new Mod();

d900bbb5fa603b8c8e55240083374fc4cd5f1b7c
mBarneto <4347791+m-barneto@users.noreply.github.com>
2024-10-12 14:00:54 -0400

    Removed pocket bugfix (i did an oopsie, gonna figure it out still)

--- src/mod.ts
+++ src/mod.ts
@@ -4,11 +4,14 @@ import { InraidControllerExtension } from "./InraidControllerExtension";
 import { ILogger } from "@spt/models/spt/utils/ILogger";
 
 class Mod implements IPreSptLoadMod {
-
     preSptLoad(container: DependencyContainer): void {
         const logger = container.resolve<ILogger>("WinstonLogger");
         container.register<InraidControllerExtension>("InraidControllerExtension", InraidControllerExtension);
         container.register("InraidController", { useToken: "InraidControllerExtension" });
+    }
+
+    public static itemsToSendToInsurance(): void {
+        
     }
 }
 
